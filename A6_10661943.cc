68311f11c61bd7534a329d0dcdb621556914f659
ifgyamfi <42804106+ifgyamfi@users.noreply.github.com>
2018-12-21 03:32:02 +0000

    Update A6_10661943.cc

--- A6_10661943.cc
+++ A6_10661943.cc
@@ -1,11 +1,44 @@
+//A C++ program to implement the Binary Search algorithm
+
 #include <iostream>
 using namespace std;
 
-// function for the binary search
-int binary(int a[], int size, int value);
+int binary(int arrNum[], int size, int value);
 
 int main() {
-	
+    int a[] = {10, 25, 28, 39, 56, 55, 87, 104};
+    int value;
+    cout << "Enter an integer you wish to find in the array: ";
+    cin >> value;
+    int result = binary(a, 8, value);
+    	if(result >= 0) {
+        	cout << "The number " << a[result] << " was found at the element with index " << result << endl;
+    	}
+    	else {
+        	cout << "The number " << value << " was not found. " << endl;
+    	}
+    return 0;
 }
 
-int 
+int binary(int arrNum[], int size, int value) {
+    int lowest = 0;
+    int highest = size - 1;
+
+    int midNum;
+
+    while (lowest <= highest) {
+        midNum = (lowest + highest) / 2;
+        if(value == arrNum[midNum]) {
+            return midNum;
+        }
+        else if (value > arrNum[midNum]) {
+            lowest = midNum + 1;
+        }
+        else {
+            highest = midNum - 1;
+        }
+    }
+
+    return -1;
+
+}
